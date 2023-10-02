1eb96e43b72e47c7de57ccdbd9f4eb0bb7ee1f8c
KyrylRyndych <53616293+KyrylRyndych@users.noreply.github.com>
2023-10-02 22:14:08 +0300

    change some bugs

--- catalog/models.py
+++ catalog/models.py
@@ -7,21 +7,20 @@ class Goods(models.Model):
         ('N', "No"),
     ]
     name = models.CharField(max_length=100)
-    photo = models.CharField(max_length=250, blank=True)
+    photo = models.CharField(max_length=250, blank=True, null=True)
     category = models.CharField(max_length=50)
     top_sel_month = models.CharField(max_length=1, choices=available)
     in_stock = models.CharField(max_length=1, choices=available)
     self_delivery = models.CharField(max_length=1, choices=available)
-    description1 = models.CharField(max_length=250, null=True)
-    description2 = models.CharField(max_length=250, null=True)
+    description1 = models.CharField(max_length=250, blank=True, null=True)
+    description2 = models.CharField(max_length=250, blank=True, null=True)
     price = models.IntegerField(default=1000)
 
     @property
     def description(self):
-
-        return f"#{self.description1}#{self.description2}"
+        return f"""#{self.description1}#
+    {self.description2}"""
 
     def __str__(self):
-        return f"{self.name} {self.description} Ціна: {self.price  }"
-
+        return f"{self.name} {self.description} Ціна: {self.price}"
 
